563614c25cf1cd5c178a703056eee62d4b7ee651
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-10-28 18:24:29 +0000

    Second Day: print statements and for loop

--- NPC.py
+++ NPC.py
@@ -1,4 +1,16 @@
-number_of_NPC = int(input("# of NPC's you want to generate: "))
-Gender_of_NPC = ["Male","Female"]
+import random #USED FOR RANDOM ATRIBUTE SELECTION
 
-while countdown > 0:
+
+number_of_NPC = int(input("# of NPC's you want to generate: "))
+
+
+Gender_of_NPC = ["Male","Female"]
+Female_name_of_NPC = ["Emily","Sofia","Abby","Jane","Mary","Sarah"]   #ATRIBUTE SECTION
+Male_Name_of_NPC = ["John","Mark","Bob","James","Frank","Odin","Cole"]
+Social_status_of_NPC = ["Peasent","Poor","Middle Class","Noble",]
+
+
+for i in range(number_of_NPC):
+    if random.choice(Gender_of_NPC) == "Male":          #PRINTS FINAL PRODUCT OF NPCs
+        print(random.choice(Male_Name_of_NPC),"\nAge:",random.randint(1,100),"\n")
+    else: print(random.choice(Female_name_of_NPC),"\nAge:",random.randint(1,100),"\n")
